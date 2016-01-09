a37ba040919b41b74ea82313d1d987ade6f71461
mgarcia2437696 <mgarcia345@student.rcc.edu>
2016-01-09 15:55:16 -0800

    Signed-off-by: mgarcia2437696 <mgarcia345@student.rcc.edu>

---
+++ Homework/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -0,0 +1,36 @@
+/* 
+    Author: Manuel Garcia
+    Created on January 5, 2016, 11:40 AM
+    Purpose:  Big C
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    //char letter='C';
+    char letter=67;
+    
+    //Output the results
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    
+    //Exit stage right
+    return 0;
+}
