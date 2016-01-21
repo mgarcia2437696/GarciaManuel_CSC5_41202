6e93cc41ef89dbb90be47854227e361b936f689f
mgarcia2437696 <mgarcia345@student.rcc.edu>
2016-01-21 11:52:22 -0800

    Signed-off-by: mgarcia2437696 <mgarcia345@student.rcc.edu>

---
+++ Class/SavingsInflation3_V2/main.cpp
@@ -0,0 +1,50 @@
+/* 
+    File:   main.cpp
+    Author: Manuel Garcia
+    Created on January 21, 2016, 11:08 AM
+    Purpose:  Fun look at savings and rule of 72
+ */
+
+//System Libraries
+#include <iostream> //I/O
+#include <iomanip>  //Formatting
+#include <cstdlib>  //Random Number 
+using namespace std;
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Problem to solve
+    cout<<endl<<"Fun look at savings and rule of 72"<<endl;
+    cout<<endl<<"califronia.municipalbonds.com/bonds.recent/"<<endl<<endl;
+  
+    //Set the random number seed
+    srand(static_cast<unsigned short>(time(0)));
+    
+    //Declare and Initialize variables
+    float balance=100;          //Initial balance $'s
+    float intRate=0.05f;        //Interest rate / year
+    float rule72=0.72f/intRate; //Rule of 72, Future Value = 2 * present value
+    
+    //Output the table heading
+    cout<<"The interest rate = "<<intRate*100<<"%"<<endl;
+    cout<<"At this interest Principle should double every";
+    cout<<rule72<<"Years"<<endl;
+    cout<<"YEAR      YEAR      BALANCE      INTEREST EARNED"<<endl;
+    cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<"0         2016"<<setw(13)<<balance<<setw(21)<<balance*intRate<<endl;
+    
+    //Calculate the Savings
+    balance*=(1+intRate);
+    int y=1,year=2017;
+    do{
+        balance*=(1+intRate);
+        intErnd=balance*intRate;
+        cout<<y<<" "<<setw(12)<<year<<setw(13)<<balance<<setw(21)<<balance*intRate<<endl;
+    }while(y<=50);
+    
+    //Exit stage right
+    return 0;
+}
--- Hmwk/Assignment_1/Savitch_9thEd_Chap1_Prob1_Practice_Prob/main.cpp
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_Prob1_Practice_Prob/main.cpp
@@ -17,7 +17,7 @@ using namespace std;
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare and initialize variables
-    unsigned int nPods; npPod; tPeas;
+    unsigned int nPods, npPod, tPeas;
     //Input data
     
     //Calculate or map inputs to outputs
@@ -30,16 +30,16 @@ int main(int argc, char** argv) {
     
     cout<<"Enter the number of peas in a pod:"<<endl;
     cin>> npPod;
-    unsigned int tPeas=nPods*npPod;
+    tPeas=nPods*npPod;
     cout<<"If you have ";
     cout<<nPods;
-    cout<<"pea pods"<<endl;
+    cout<<" pea pods"<<endl;
     cout<<"and ";
     cout<<npPod;
     cout<<" peas in each pod, then"<<endl;
     cout<<"you have ";
     cout<< tPeas;
-    cout<<"peas in all the pods."<<endl;
+    cout<<" peas in all the pods."<<endl;
     
     //Exit stage right
     return 0;
